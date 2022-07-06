8e342e253f8ac38011edfc4bfd969b0132c322b7
Taranchuk <nfhfyxer@gmail.com>
2022-07-06 16:09:46 +0300

    update

--- 1.3/Source/SyntheticGeneticsMod.cs
+++ 1.3/Source/SyntheticGeneticsMod.cs
@@ -4,14 +4,74 @@ using System.Linq;
 using HarmonyLib;
 using RimWorld;
 using System.Collections.Generic;
+using GeneticRim;
 
 namespace SyntheticGenetics
 {
+    [DefOf]
+    public static class SG_DefOf
+    {
+        public static RecipeDef SG_GenoframeRecipe;
+    }
+
     public class SyntheticGeneticsMod : Mod
     {
         public SyntheticGeneticsMod(ModContentPack content) : base(content)
         {
             new Harmony("SyntheticGenetics.Mod").PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(GenRecipe), "MakeRecipeProducts")]
+    public static class MakeRecipeProducts_Patch
+    {
+        private static IEnumerable<Thing> Postfix(IEnumerable<Thing> __result, RecipeDef recipeDef, Pawn worker, List<Thing> ingredients, Thing dominantIngredient, IBillGiver billGiver, Precept_ThingStyle precept = null)
+        {
+            foreach (var result in __result)
+            {
+                yield return result;
+            }
+            if (recipeDef == SG_DefOf.SG_GenoframeRecipe)
+            {
+                float efficiency = ((recipeDef.efficiencyStat != null) ? worker.GetStatValue(recipeDef.efficiencyStat) : 1f);
+                if (recipeDef.workTableEfficiencyStat != null)
+                {
+                    Building_WorkTable building_WorkTable = billGiver as Building_WorkTable;
+                    if (building_WorkTable != null)
+                    {
+                        efficiency *= building_WorkTable.GetStatValue(recipeDef.workTableEfficiencyStat);
+                    }
+                }
+
+                ThingDefCountClass thingDefCountClass = new ThingDefCountClass { count = 1, thingDef = Core.genoframes.RandomElement() };
+                Thing thing = ThingMaker.MakeThing(stuff: (!thingDefCountClass.thingDef.MadeFromStuff) ? null : dominantIngredient.def, def: thingDefCountClass.thingDef);
+                thing.stackCount = Mathf.CeilToInt((float)thingDefCountClass.count * efficiency);
+                if (dominantIngredient != null && recipeDef.useIngredientsForColor)
+                {
+                    thing.SetColor(dominantIngredient.DrawColor, reportFailure: false);
+                }
+                CompIngredients compIngredients = thing.TryGetComp<CompIngredients>();
+                if (compIngredients != null)
+                {
+                    for (int l = 0; l < ingredients.Count; l++)
+                    {
+                        compIngredients.RegisterIngredient(ingredients[l].def);
+                    }
+                }
+                CompFoodPoisonable compFoodPoisonable = thing.TryGetComp<CompFoodPoisonable>();
+                if (compFoodPoisonable != null)
+                {
+                    if (Rand.Chance(worker.GetRoom()?.GetStat(RoomStatDefOf.FoodPoisonChance) ?? RoomStatDefOf.FoodPoisonChance.roomlessScore))
+                    {
+                        compFoodPoisonable.SetPoisoned(FoodPoisonCause.FilthyKitchen);
+                    }
+                    else if (Rand.Chance(worker.GetStatValue(StatDefOf.FoodPoisonChance)))
+                    {
+                        compFoodPoisonable.SetPoisoned(FoodPoisonCause.IncompetentCook);
+                    }
+                }
+                yield return GenRecipe.PostProcessProduct(thing, recipeDef, worker, precept);
+            }
         }
     }
 
@@ -24,9 +84,7 @@ namespace SyntheticGenetics
             if (!generated)
             {
                 generated = true;
-                var genomes = DefDatabase<ThingDef>.AllDefs.Where((ThingDef x) => x.thingCategories?.Any(x => x.defName.Contains("GR_GeneticMaterial")) ?? false).ToList();
-                var genoframes = DefDatabase<ThingDef>.AllDefs.Where((ThingDef x) => x.thingCategories?.Any(x => x.defName.Contains("GR_Genoframes")) ?? false).ToList();
-                foreach (var genome in genomes)
+                foreach (var genome in DefDatabase<ThingDef>.AllDefs.Where((ThingDef x) => x.thingCategories?.Any(x => x.defName.Contains("GR_GeneticMaterial")) ?? false))
                 {
                     var def = GetRecipeDef(genome);
                     def.PostLoad();
